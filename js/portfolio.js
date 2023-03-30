2b0d994248de84263d27882b135dd06b341ba66d
AchrafGroiez <112225386+AchrafGroiez@users.noreply.github.com>
2023-03-30 13:22:34 -0400

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,14 +3,14 @@ const change2 = document.getElementById("change2")
 change1.checked = true;
 change1.addEventListener('click', () => {
     if(change1.checked){
-        imgchangein.src = "/img_video/TP04.jpg"
+        imgchangein.src = "/TP-Final-Session1/img_video/TP04.jpg"
         changediv.className = "divwhite"
         tp04.className = "yes"
         tp02.className = "no"
     }
 
     else{
-        imgchangein.src = "/img_video/TP02.jpg"
+        imgchangein.src = "/TP-Final-Session1/img_video/TP02.jpg"
         changediv.className = "divblack"
         tp04.className = "no"
         tp02.className = "yes"
@@ -20,14 +20,14 @@ change1.addEventListener('click', () => {
 
 change2.addEventListener('click', () => {
     if(change2.checked){
-        imgchangein.src = "/img_video/TP02.jpg"
+        imgchangein.src = "/TP-Final-Session1/img_video/TP02.jpg"
         changediv.className = "divblack"
         tp04.className = "no"
         tp02.className = "yes"
     }
 
     else{
-        imgchangein.src = "/img_video/TP04.jpg"
+        imgchangein.src = "/TP-Final-Session1/img_video/TP04.jpg"
         changediv.className = "divwhite"
         tp04.className = "yes"
         tp02.className = "no"
